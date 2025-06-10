5fffa7b9ca436643fb4b68428b172f72a06766d2
Siti Mariam <stmrymmm1124@gmail.com>
2025-06-10 23:25:24 +0800

    Menambah fk mapel ke tabel guru

--- database/migrations/2025_04_19_060000_create_teachers_table.php
+++ database/migrations/2025_04_19_060000_create_teachers_table.php
@@ -16,10 +16,12 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('class_id');
             $table->unsignedBigInteger('major_id');
+            $table->unsignedBigInteger('sub_id');
             $table->string('nip', 20)->nullable()->unique(); 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('class_id')->references('id')->on('classes')->onDelete('cascade');
-            $table->foreign('major_id')->references('id')->on('majors')->onDelete('cascade');      
+            $table->foreign('major_id')->references('id')->on('majors')->onDelete('cascade');   
+            $table->foreign('sub_id')->references('id')->on('subjects')->onDelete('cascade');         
             $table->timestamps();
         });
     }
